3ca660f215cf673e3703e4b17ebab63891e02c09
Carlos Reyes <carlosreyes8436@gmail.com>
2022-04-14 20:46:58 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+
+function compilecss() {
+    return src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('dist/css'))
+}
+
+function minify() {
+    return src('src/scss/app.scss')
+        const paths = [
+            autoprefixer(),
+            cssnano()
+        ]
+        .pipe(sass())
+        .pipe(postcss(paths))
+        .pipe(dest('dist/css'))
+}
+
+function watchArchives() {
+    watch('src/scss/**/*.scss', compilecss);
+}
+
+exports.default = watchArchives
